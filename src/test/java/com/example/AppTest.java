09cf90b05ce26d2ff6b4eab33f0fc9e450163354
anazalu <you@example.com>
2024-11-23 13:14:04 +0200

    add dependencies

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,8 +1,8 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue; // JUnit 5 Assertions
+import org.junit.jupiter.api.Test; // JUnit 5 Test annotation
 
-import org.junit.Test;
 
 /**
  * Unit test for simple App.
