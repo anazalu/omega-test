cf53454664f937c6e42a1505d19bb8c54bb7fdc7
anazalu <you@example.com>
2024-11-23 14:08:42 +0200

    add webdriver

---
+++ README.md
@@ -0,0 +1,33 @@
+# Software under test:
+
+Mūsu komandas uzdevums ir nodrošināt, ka šī platforma ir droša, uzticama un patīkama lietošanai mūsu darbiniekiem.
+
+https://omega-vismatestingapp.azurewebsites.net/ 
+
+1. Veikt manuālo aplikācijas testu un aprakstīt testēšanas rezultātus.
+
+2. Identificēt svarīgākos testēšanas scenārijus.
+
+3. Uzrakstīt  automatizētos testus  2-3 svarīgākajiem scenārijiem.
+Tehnoloģijas/valodas/rīkus var izvēlēties pēc saviem ieskatiem.
+
+4. Veikt  API validācijas testus tiem endpointiem, kuri ir aprakstīti swagerī: 
+
+https://omega-vismatestingapp.azurewebsites.net/swagger/index.html
+
+Vismaz 5 testpiemēri POST pieprasījumam
+
+2 testpiemēri DELETE pieprasījumam + paskaidrojums, kā tika verificēts, ka dati tika izdzēsti
+
+Papilduzdevums:
+
+2-3 ieteikumi lietotāja pieredzes uzlabojumiem aplikācijā.
+
+# Useful commands
+
+## To run tests:
+
+```
+mvn test
+```
+
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,20 +1,40 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.assertTrue; // JUnit 5 Assertions
-import org.junit.jupiter.api.Test; // JUnit 5 Test annotation
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import dev.failsafe.internal.util.Assert;
 
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+public class AppTest {
+    private static WebDriver driver = null;
+
+    @BeforeAll
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("https://magento.softwaretestingboard.com/");
     }
+
+    @Test
+    public void testTitle() {
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Home Page";
+        assertEquals(expectedTitle, actualTitle, "Title mismatch");
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }  
 }
